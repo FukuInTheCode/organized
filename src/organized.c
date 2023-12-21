237510dc9b295672e1c6c0e091e38d41bafab973
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:56:02 +0100

    [~] free the list

--- src/organized.c
+++ src/organized.c
@@ -7,9 +7,19 @@
 
 #include "my.h"
 
+static int free_list(my_item_t **items)
+{
+    for (; *items; del_el(items, (*items)->id));
+    return 0;
+}
+
 int main(int argc, char **argv, char **envp)
 {
     my_item_t *items = NULL;
+    int return_value = workshop_shell(&items);
 
-    return workshop_shell(&items);
+    if (argc != 1)
+        return 84;
+    free_list(&items);
+    return return_value;
 }
